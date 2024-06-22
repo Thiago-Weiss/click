3213623880be6657a8646bbb38f5181119f0e0a5
Thiago-Weiss <137117035+Thiago-Weiss@users.noreply.github.com>
2024-06-22 17:19:52 -0300

    att

--- testes/2.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-
-root = Tk()
-var = StringVar()
-label = Message(root, textvariable=var, relief=RAISED, aspect=300)  
-var.set("Hey!? How are you doing? aaaaaaaaaaa")
-label.pack(expand=True, fill='both')
-
-root.mainloop()
